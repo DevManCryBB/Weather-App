9f66ab40d9643ada4fd915a353a895c5a4cd9c6f
DevManCryBB <veronicagriggs3@gmail.com>
2023-04-11 01:57:05 -0700

    adding api keys

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,19 @@
-var apiKey = "849a8c3ffd4ce5316f7c7d1951074537"
-var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat={47.6062}&lon={122.3321}q=city&appid={849a8c3ffd4ce5316f7c7d1951074537}"
+var apiKey = "9c249410be92c2fa518fe16ca01c7790"
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Seattle&appid={9c249410be92c2fa518fe16ca01c7790}"
 var city;
 var searchButton = document.getElementById
 
 fetch(queryURL)
- getSelection
-  then(function (response) {
+  .then(function (response) {
     return response.json();
   })
-  then(function (data) {
+  .then(function (data) {
     console.log(data);
   });
 
-searchButton.on("click", function (event) {
-    event.preventDefault();
-});
+// searchButton.on("click", function (event) {
+//     event.preventDefault();
+// });
+
+// inplace of "weather"
+// forecast?lat={47.6062}&lon={122.3321}
