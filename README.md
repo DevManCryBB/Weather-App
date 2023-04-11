b1b5f4a946fd5b4bdd918555e40eff8eca20ba6e
DevManCryBB <veronicagriggs3@gmail.com>
2023-04-11 01:13:36 -0700

    adding readme

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# sixth-challenge
-Weather App
+# Weather App
+
+## Description
+
+I created this app in order for the user to easily find out what the weather is like in their area or an area they may be traveling to in the near future, within a five day frorecast. An app like this can make it easy to plan around the weather so the use may accomodate themselves accordingly on their travels.
+
+## Usage
+
+To use this application simply type in the city you wish to see the forecast of and hit the "search" button. Your five day forecast will be immediately populated on screen.
+secondarily the user may find their city listed in the already existing buttons below the general search bar.
+
+![alt text](assets/images/screenshot.png)
+
+## Credits
+
+openweathermap.org
+
--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,17 @@
-// var apiKey = "849a8c3ffd4ce5316f7c7d1951074537"
-// var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}q=city&appid={849a8c3ffd4ce5316f7c7d1951074537}"
-// var city;
-// var searchButton = document.getElementById
+var apiKey = "849a8c3ffd4ce5316f7c7d1951074537"
+var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat={47.6062}&lon={122.3321}q=city&appid={849a8c3ffd4ce5316f7c7d1951074537}"
+var city;
+var searchButton = document.getElementById
 
-// fetch(queryURL)
-//  getSelection
-//   then(function (response) {
-//     return response.json();
-//   })
-//   then(function (data) {
-//     console.log(data);
-//   });
+fetch(queryURL)
+ getSelection
+  then(function (response) {
+    return response.json();
+  })
+  then(function (data) {
+    console.log(data);
+  });
 
-// searchButton.on("click", function (event) {
-//     event.preventDefault();
-// });
-
-console.log("hello")
+searchButton.on("click", function (event) {
+    event.preventDefault();
+});
