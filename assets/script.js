6db843213e824003ab78f723597048312a8bc5a8
DevManCryBB <veronicagriggs3@gmail.com>
2023-04-11 01:02:46 -0700

    updates

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,19 @@
-var apiKey = "849a8c3ffd4ce5316f7c7d1951074537"
-var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}q=city&appid={849a8c3ffd4ce5316f7c7d1951074537}"
-var city;
-var searchButton = document.getElementById
+// var apiKey = "849a8c3ffd4ce5316f7c7d1951074537"
+// var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}q=city&appid={849a8c3ffd4ce5316f7c7d1951074537}"
+// var city;
+// var searchButton = document.getElementById
 
-fetch(queryURL)
- getSelection
-  then(function (response) {
-    return response.json();
-  })
-  then(function (data) {
-    console.log(data);
-  });
+// fetch(queryURL)
+//  getSelection
+//   then(function (response) {
+//     return response.json();
+//   })
+//   then(function (data) {
+//     console.log(data);
+//   });
 
-searchButton.on("click", function (event) {
-    event.preventDefault();
-});
+// searchButton.on("click", function (event) {
+//     event.preventDefault();
+// });
+
+console.log("hello")
