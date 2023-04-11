a209412246326d197c5e5bf95c9f54f2672618b4
DevManCryBB <veronicagriggs3@gmail.com>
2023-04-11 16:09:21 -0700

    updates

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,39 @@
-var apiKey = "9c249410be92c2fa518fe16ca01c7790"
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Seattle&appid={9c249410be92c2fa518fe16ca01c7790}"
+var apiKey = "9c249410be92c2fa518fe16ca01c7790";
 var city;
-var searchButton = document.getElementById
+var searchButton = document.getElementById("generate");
 
-fetch(queryURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
+function searchField() {
+  var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  fetch(queryURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+  fiveDay();
+}
 
-// searchButton.on("click", function (event) {
-//     event.preventDefault();
-// });
-
-// inplace of "weather"
-// forecast?lat={47.6062}&lon={122.3321}
+function fiveDay() {
+  var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;
+  fetch(queryURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      for (let i = 0; i < data.list.length; i += 8) {
+        var forecastCard = `
+<h1>Temp: ${data.list[i].main.temp}</h1>
+<p class="someClass">Some other value: ${data.list[i].main}</p>
+`;
+        document.getElementById("forecast").innerHTML += forecastCard;
+      }
+      console.log(data);
+    });
+}
+searchButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  event.stopImmediatePropagation();
+  city = document.getElementById("mySearch").value;
+  searchField();
+});
